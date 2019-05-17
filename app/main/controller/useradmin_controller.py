7a2cfc1385e04c10dbb5eb9888a4aa5a7b2b212b
shainamarie <sweetmarie980@gmail.com>
2019-05-18 02:21:10 +0800

    search function optimized again

--- app/main/controller/useradmin_controller.py
+++ app/main/controller/useradmin_controller.py
@@ -3,11 +3,7 @@ from flask_restplus import Resource
 
 from ..util.dto import UserAdminDto, PasswordAdminDto
 from ..util.decoratoradmin import token_required
-<<<<<<< HEAD
 from ..service.useradmin_service import save_new_user, get_all_users, get_a_user, delete_user, update_user, update_password, search_by_user
-=======
-from ..service.useradmin_service import save_new_user, get_all_users, get_a_user, delete_user, update_user, update_password, search_by_username, search_by_fullname, search_by_public_id, search_by_role
->>>>>>> a4a50f6df42b7c12e2eac01c26aadccccbb04cc3
 
 api = UserAdminDto.api
 _user = UserAdminDto.user
@@ -93,12 +89,10 @@ class UserPassword(Resource):
 			api.abort(404)
 		else:
 			return user
-			
-<<<<<<< HEAD
+
+
+
 @api.route('/search/<search_user>')			
-=======
-@api.route('/search/by_username/<searchterm_user>')			
->>>>>>> a4a50f6df42b7c12e2eac01c26aadccccbb04cc3
 class SearchByUsername(Resource):
 	@api.doc('search by username, name, public_id, role')
 	@api.response(200, 'Results found.')
@@ -107,34 +101,3 @@ class SearchByUsername(Resource):
 		results = search_by_user(search_user)
 		return results
 
-<<<<<<< HEAD
-=======
-@api.route('/search/by_name/<searchterm_name>')			
-class SearchByName(Resource):		
-	@api.doc('search by name')
-	@api.response(200, 'Results found.')
-	@api.marshal_list_with(_user, envelope='data')
-	def get(self, searchterm_name):
-		results = search_by_fullname(searchterm_name)
-		return results
-
-@api.route('/search/by_id/<searchterm_id>')			
-class SearchByPublicId(Resource):
-	@api.doc('search by public id')
-	@api.response(200, 'Results found.')
-	@api.marshal_list_with(_user, envelope='data')
-	def get(self, searchterm_id):
-		results = search_by_public_id(searchterm_id)
-		return results
-		
-@api.route('/search/by_role/<searchterm_role>')			
-class SearchByRole(Resource):
-	@api.doc('search by admin role')
-	@api.response(200, 'Results found.')
-	@api.marshal_list_with(_user, envelope='data')
-	def get(self, searchterm_role):
-		results = search_by_role(searchterm_role)
-		return results
-#Search 'Main Admin' to search for main admins, 'relief admin' for relief admins, 'social worker admin' for socil worker admins
-#All search functions are case insensitive. you can search for 'Main' or 'maiN' or 'MaIn', same result.
->>>>>>> a4a50f6df42b7c12e2eac01c26aadccccbb04cc3
