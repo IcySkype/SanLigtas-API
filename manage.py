28ed47c57553d6883c557d93de08551776c52df1
IcySkype <skytrizero@gmail.com>
2020-01-18 00:03:37 +0800

    manage.py fix

--- manage.py
+++ manage.py
@@ -36,7 +36,7 @@ def init():
 @manager.command
 def run():
 	port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
 
 @manager.command
 def test():
